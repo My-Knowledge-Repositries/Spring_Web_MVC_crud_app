afb21dd90149b676b6a5da8c86b7f50ae6447396
oshadaera68 <mindartlk010@gmail.com>
2023-05-11 20:55:20 +0530

    updated

--- src/main/java/com/devstack/app/controller/CustomerController.java
+++ src/main/java/com/devstack/app/controller/CustomerController.java
@@ -1,7 +1,12 @@
 package com.devstack.app.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/customer")
 public class CustomerController {
+    public String testEndPoint(){
+        return "hello client, i am the customer here!";
+    }
 }
